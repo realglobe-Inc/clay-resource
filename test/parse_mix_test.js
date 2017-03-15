d14ff6c59b89b8b1f8b400dad532fef9bba2768d
Taka Okunishi <okunishinish@gmail.com>
2017-03-15 12:03:49 +0900

    Add parse mix

---
+++ test/parse_mix_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for parseMix.
+ * Runs with mocha.
+ */
+'use strict'
+
+const parseMix = require('../lib/mixins/parse_mix.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('parse-mix', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Parse mix', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
