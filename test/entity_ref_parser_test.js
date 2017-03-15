9d85bb9208df3d1e0e3e20662c14025788e8b1fc
rg-ci <ci@realglobe.jp>
2017-03-15 06:13:04 +0000

    [ci skip] Travis CI committed after build

---
+++ test/entity_ref_parser_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for entityRefParser.
+ * Runs with mocha.
+ */
+'use strict'
+
+const entityRefParser = require('../lib/parsers/entity_ref_parser.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('entity-ref-parser', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Entity ref parser', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
