c4ecbf6456ab3b9bc47cb1832ef1724754b279f3
Taka Okunishi <okunishinish@gmail.com>
2017-04-19 10:46:06 +0900

    entityでの検索を実現した
    https://github.com/realglobe-Inc/jk-db/issues/7

---
+++ test/condition_mix_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for conditionMix.
+ * Runs with mocha.
+ */
+'use strict'
+
+const conditionMix = require('../lib/mixins/condition_mix.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('condition-mix', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Condition mix', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
