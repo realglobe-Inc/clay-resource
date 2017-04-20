f0c60b8abf6c105e4eaa82cc38ab01395cf970b6
rg-ci <ci@realglobe.jp>
2017-04-20 05:43:45 +0000

    [ci skip] Travis CI committed after build

---
+++ test/as_entity_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for asEntity.
+ * Runs with mocha.
+ */
+'use strict'
+
+const asEntity = require('../lib/helpers/as_entity.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('as-entity', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('As entity', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
