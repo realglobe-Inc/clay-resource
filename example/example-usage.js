ad6fc7b2e6a4bcea1d46d88bca8e94c9f2cbfd56
okunishinishi <okunishinishi@gmail.com>
2017-10-03 14:46:43 +0900

    Add handlers

--- example/example-usage.js
+++ example/example-usage.js
@@ -1,20 +1,20 @@
 'use strict'
 
-const { fromDriver } = require('clay-resource')
+const {fromDriver} = require('clay-resource')
 const clayDriverMemory = require('clay-driver-memory')
 
 async function tryExample () {
-  let driver = clayDriverMemory()
-  let Product = fromDriver(driver, 'Product')
+  const driver = clayDriverMemory()
+  const Product = fromDriver(driver, 'Product')
 
   {
-    let product01 = await Product.create({ name: 'Awesome Box', price: 100 })
-    let { id } = product01
-    await Product.update(id, { price: 200 })
+    const product01 = await Product.create({name: 'Awesome Box', price: 100})
+    const {id} = product01
+    await Product.update(id, {price: 200})
 
-    let { meta, entities } = await Product.list({
-      filter: { price: { $gt: 100 } },
-      page: { size: 25, number: 1 }
+    const {meta, entities} = await Product.list({
+      filter: {price: {$gt: 100}}, // Supported operators: "$gt", "$gte", "$lt", "$lte", "$like", "$in" ...etc
+      page: {size: 25, number: 1}
     })
     console.log('Found products:', entities, 'total:', meta.total)
     await Product.destroy(id)
